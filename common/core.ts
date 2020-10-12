eb1654e3ffdb88fb857006d2f393fe4e950bb6b8
luncheon <takeuck+github@gmail.com>
2020-10-12 16:01:41 +0900

    optimize

--- common/core.ts
+++ common/core.ts
@@ -6,41 +6,32 @@ class BitTable {
   }
 
   get(x: number, y: number): 1 | 0 {
-    const { X, Y } = this
-    x = x % X
-    x < 0 && (x = x + X)
-    y = y % Y
-    y < 0 && (y = y + Y)
-    const index = y * X + x
-    return this.buffer[(index / 32) | 0] & (1 << index % 32) ? 1 : 0
+    const { X, Y, buffer } = this
+    const _x = x < 0 ? (x % X) + X : x % X
+    const _y = y < 0 ? (y % Y) + Y : y % Y
+    const index = _y * X + _x
+    return buffer[index >> 5] & (1 << (index & 31)) ? 1 : 0 // (*1)
   }
 
   set(x: number, y: number, value: unknown) {
     const index = y * this.X + x
     if (value) {
-      this.buffer[(index / 32) | 0] |= 1 << index % 32
+      this.buffer[index >> 5] |= 1 << (index & 31) // (*1)
     } else {
-      this.buffer[(index / 32) | 0] &= ~(1 << index % 32)
+      this.buffer[index >> 5] &= ~(1 << (index & 31)) // (*1)
     }
   }
-}
 
-const rand = (() => {
-  let x = (Math.random() * 2147483647) | 0
-  return () => {
-    x ^= x << 13
-    x ^= x >> 17
-    x ^= x << 5
-    return x
-  }
-})()
+  // (*1)
+  //  n >> 5 === (n / 32) | 0
+  //  n & 31 === (n % 32) | 0
+}
 
 export const init = (X: number, Y: number, aliveRatio = 0.25) => {
   const bits = new BitTable(X, Y)
-  const aliveThreshold = (100 * aliveRatio) | 0
   for (let y = 0; y < Y; y++) {
     for (let x = 0; x < X; x++) {
-      if ((rand() % 50) + 50 < aliveThreshold) {
+      if (Math.random() < aliveRatio) {
         bits.set(x, y, 1)
       }
     }
